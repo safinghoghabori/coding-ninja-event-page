14187d58bd8f97fda7bcd53fe2ea8e1e604b20f0
safin ghoghabori <safinghoghabori65@gmail.com>
2021-06-15 09:36:46 +0530

    cards changed

---
+++ cn-assignment/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+// componetns imports
+import './header.css'
+
+const Header = () => {
+    return(
+        <header className='header-area'>
+            <h1>Events & News</h1>
+            <h5>Learn, Compete and Grow</h5>
+        </header>
+    )
+}
+
+export default Header;
