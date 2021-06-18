9b0164944cf89fb24fb2ae9dd7a4e89541e3b277
Safin Ghoghabori <safinghoghabori65@gmail.com>
2021-06-18 16:10:36 +0000

    events card changed and responsiveness added

--- src/components/EventsCard/EventsCard.jsx
+++ src/components/EventsCard/EventsCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import registerImg from "../../images/registerImg.png";
 
 // reactstrap
-import {Card} from 'react-bootstrap'
+import { Card } from 'react-bootstrap'
 import { Button } from "react-bootstrap";
 
 // components import
@@ -12,13 +12,12 @@ import eventImg3 from '../../images/eventImg3.jpg'
 import './eventcard.css'
 
 const EventsCard = () => {
-    return(
-        <>
-    
-    <Card style={{ width: '25rem',minHeight:'30rem' }}>
-        <Card.Img variant="top" src={eventImg3}/>
-        <Card.Body>
-            <Card.Title>Roadmap to Crack Placements & Internships | Bundelkhand Institute Of Engineering and Technology</Card.Title>
+    return (
+
+        <Card style={{ width: '25rem', minHeight: '30rem' }} className='card'>
+            <Card.Img variant="top" src={eventImg3} />
+            <Card.Body>
+                <Card.Title>Roadmap to Crack Placements & Internships | Bundelkhand Institute Of Engineering and Technology</Card.Title>
                 <Card.Text>
                     Give your career a head-start and know how you can get closer to your dream job by refining your coding skills.
                     <div className='sub__tags'>
@@ -27,7 +26,7 @@ const EventsCard = () => {
                         <div className='sub__tags__detail'>Interview Preparation</div>
                     </div>
                 </Card.Text>
-                <hr/>
+                <hr />
                 <Card.Title className='sub__details'>
                     <div className='sub__details__info'>
                         <p>Starts On</p>
@@ -42,14 +41,13 @@ const EventsCard = () => {
                         <span>Online</span>
                     </div>
                 </Card.Title>
-                <hr/>
+                <hr />
                 <div className='card__footer'>
                     <p className='bottom__text'>Hurry up! <b>45 people</b> already registered.</p>
                     <img src={registerImg} alt='register image' />
                 </div>
-        </Card.Body>
-    </Card>
-    </>
+            </Card.Body>
+        </Card>
     )
 }
 
--- src/pages/Events/Events.jsx
+++ src/pages/Events/Events.jsx
@@ -1,6 +1,6 @@
-import React,{useEffect,useState} from "react";
+import React, { useEffect, useState } from "react";
 import axios from 'axios'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 // fontawesome
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -15,26 +15,26 @@ import './events.css'
 import Card from "../../components/EventsCard/EventsCard";
 
 const Events = () => {
-    const [eventTags,setEventTags] = useState('');
+    const [eventTags, setEventTags] = useState('');
 
-    const [cardsDetails,setCardsDetails] = useState([]);
-    
-    const [mainCategory,setMainCategory] = useState('');
-    const [subCategory,setSubCategory] = useState('');
-    const [tags,setTags] = useState([]);
+    const [cardsDetails, setCardsDetails] = useState([]);
+
+    const [mainCategory, setMainCategory] = useState('');
+    const [subCategory, setSubCategory] = useState('');
+    const [tags, setTags] = useState([]);
 
     useEffect(() => {
         axios.get('https://api.codingninjas.com/api/v3/event_tags')
-        .then((res) => {
-            setEventTags(res.data.data.tags)
-        }).catch((error) => console.log(error))
-    },[])
+            .then((res) => {
+                setEventTags(res.data.data.tags)
+            }).catch((error) => console.log(error))
+    }, [])
 
     useEffect(() => {
         axios.get(`https://api.codingninjas.com/api/v3/events?event_category=${mainCategory}&event_sub_category=${subCategory}&tag_list=${tags}&offset=0`)
             .then((res) => console.log(res))
             .catch((error) => console.log(error))
-    },[mainCategory])
+    }, [mainCategory])
 
     const onMainCategory = (e) => {
         // setMainCategory(e.target.value)
@@ -45,64 +45,64 @@ const Events = () => {
         console.log(e.target.value)
     }
 
-    let arr= [];
+    let arr = [];
     const onTags = (e) => {
         arr.push(e.target.value)
         setTags(arr)
         console.log(arr)
     }
 
-    return(
+    return (
         <>
-        <div className='wrapper'>
-            <div className="event__background">
-                <div className='event__category'>
-                    <div className='event__category__links'>
-                        <FontAwesomeIcon icon={faCalendarDay} style={{fontSize:'1.2rem'}} />
-                        <button className='links' value='ALL_EVENTS' onClick={onMainCategory} >All Events</button>
+            <div className='wrapper'>
+                <div className="event__background">
+                    <div className='event__category'>
+                        <div className='event__category__links ediv'>
+                            <FontAwesomeIcon icon={faCalendarDay} style={{ fontSize: '1.2rem' }} />
+                            <button className='links' value='ALL_EVENTS' onClick={onMainCategory} >All Events</button>
+                        </div>
+                        <div className='event__category__links ediv'>
+                            <FontAwesomeIcon icon={faLaptopCode} style={{ fontSize: '1.2rem' }} />
+                            <button className='links' value='WEBINARS' onClick={onMainCategory}>Webinars</button>
+                        </div>
+                        <div className='event__category__links ediv'>
+                            <FontAwesomeIcon icon={faCode} style={{ fontSize: '1.2rem' }} />
+                            <button className='links' value='CODING_EVENTS' onClick={onMainCategory}>Coding Events</button>
+                        </div>
+                        <div className='event__category__links ediv'>
+                            <FontAwesomeIcon icon={faRoute} style={{ fontSize: '1.2rem' }} />
+                            <button className='links' value='BOOTCAMP_EVENTS' onClick={onMainCategory}>Bootcamp Events</button>
+                        </div>
+                        <div className='event__category__links ediv'>
+                            <FontAwesomeIcon icon={faPlaceOfWorship} style={{ fontSize: '1.2rem' }} />
+                            <button className='links' value='WORKSHOPS' onClick={onMainCategory}>Workshops</button>
+                        </div>
                     </div>
-                    <div className='event__category__links'>
-                        <FontAwesomeIcon icon={faLaptopCode} style={{fontSize:'1.2rem'}}  />
-                        <button className='links' value='WEBINARS' onClick={onMainCategory}>Webinars</button>
-                    </div>
-                    <div className='event__category__links'>
-                        <FontAwesomeIcon icon={faCode}  style={{fontSize:'1.2rem'}}  />
-                        <button className='links' value='CODING_EVENTS' onClick={onMainCategory}>Coding Events</button>
-                    </div>
-                    <div className='event__category__links'>
-                        <FontAwesomeIcon icon={faRoute}  style={{fontSize:'1.2rem'}}  />
-                        <button className='links' value='BOOTCAMP_EVENTS' onClick={onMainCategory}>Bootcamp Events</button>
-                    </div>
-                    <div className='event__category__links'>
-                        <FontAwesomeIcon icon={faPlaceOfWorship}  style={{fontSize:'1.2rem'}}  />
-                        <button className='links' value='WORKSHOPS' onClick={onMainCategory}>Workshops</button>
+                    <hr />
+
+                    <div className='event__sub__category'>
+                        <div className='event__category__links ediv'>
+                            <button className='links' onClick={onSubCategory} value='Upcoming'>Upcoming</button>
+                        </div>
+                        <div className='event__category__links ediv'>
+                            <button className='links' onClick={onSubCategory} value='Archived'>Archived</button>
+                        </div>
+                        <div className='event__category__links ediv'>
+                            <button className='links' onClick={onSubCategory} value='ALL_TIME_FAVOURITES'>All Time Favorites</button>
+                        </div>
                     </div>
                 </div>
-                <hr/>
-            
-                <div className='event__sub__category'>
-                    <div className='event__category__links'>
-                        <button className='links' onClick={onSubCategory} value='Upcoming'>Upcoming</button>
-                    </div>
-                    <div className='event__category__links'>
-                        <button className='links' onClick={onSubCategory} value='Archived'>Archived</button>
-                    </div>
-                    <div className='event__category__links'>
-                        <button className='links' onClick={onSubCategory} value='ALL_TIME_FAVOURITES'>All Time Favorites</button>
-                    </div>
+                <div className='taglist'>
+                    {eventTags && eventTags.map((tag, i) =>
+                        <div className='event__category__links tags' key={i}>
+                            <button className='links' onClick={onTags} value={tag} >{tag}</button>
+                        </div>
+                    )}
+                </div><hr />
+                <div className='event__cards'>
+                    <Card /><Card /><Card />
                 </div>
             </div>
-            <div className='taglist'>
-                {eventTags && eventTags.map((tag,i) => 
-                    <div className='event__category__links tags' key={i}>
-                        <button className='links' onClick={onTags} value={tag} >{tag}</button>
-                    </div>
-                )}
-            </div><hr/>
-            <div className='event__cards'>
-               <Card /><Card /><Card />
-            </div>
-        </div>
         </>
     )
 }
